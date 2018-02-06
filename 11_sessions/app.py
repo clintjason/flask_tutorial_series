bd95c11536ce6f755825482399354e2705fb591b
clintjason <ngassajason@yahoo.fr>
2018-02-06 04:46:54 +0100

    session module

--- 11_sessions/app.py
+++ 11_sessions/app.py
@@ -1,14 +1,15 @@
 from flask import Flask, session, redirect, url_for, escape, request
 import os
+import sys
 
 app = Flask(__name__)
 
-app.secret_key = os.urandom(16)
+app.secret_key = os.urandom(16)		# random number generator
 
 @app.route('/')
 def index():
 	if 'username' in session:
-		return 'Logged in as %s' % escape(session['username'])
+		return 'Logged in as %s' % escape(session['username'])	# escape() is used to escape characters
 	return 'You are not logged in'
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -25,8 +26,15 @@ def login():
 @app.route('/logout')
 def logout():
 	# remove the username from the session if it's there
-	session.pop('username', None)
+	value = session.pop('username', None)
+	print('The session key deleted is ' + value)
 	return redirect(url_for('index'))
 
-if __name__ == '__main__':
-	app.run(debug=True)
+'''
+According to Flask's API their Session class is a wrapper around a python Dict.
+According to the python documentation for dict.pop():
+    pop(key[, default])
+If key is in the dictionary, remove it and return its value, else return default.
+If default is not given and key is not in the dictionary, a KeyError is raised.
+In this case the tutorial asks you to pass in None as the default value.
+'''
